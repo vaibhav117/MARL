998ca94a038396a7ea6b53e2400496ba11853e56
vaibhav117 <vaibhavmathur117@gmail.com>
2021-12-19 03:39:20 -0500

    Adding CPU scripts

---
+++ config_script_cpu.sh
@@ -0,0 +1,1 @@
+python train.py --device cpu --project_name Deep_Learning_Project_2 --experiment_name Independent_DQNs --spawn_rate 1 --num_knights 1 --num_archers 0 --batch_size 256 --network_update_freq 10 --sync_target_network_freq 1000 --eps_decay 0.0001
