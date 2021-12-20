8024c1d6f8fb7a958f477aff5c779f050e401a48
vaibhav117 <vaibhavmathur117@gmail.com>
2021-12-19 23:51:23 -0500

    Single agent runs on Priority Queue

--- config_script_cpu.sh
+++ config_script_cpu.sh
@@ -1,1 +1,1 @@
-python train.py --device cpu --project_name Deep_Learning_Project_2 --experiment_name PriorityRB_Independent_DQNs --num_knights 1 --num_archers 0 --batch_size 512 --lr 0.0001 --network_update_freq 10 --sync_target_network_freq 1000 --eps_decay 0.0001 --spawn_rate 10
+python train.py --device cpu --project_name Deep_Learning_Project_2 --experiment_name PriorityRB_Independent_DQNs --num_knights 0 --num_archers 1 --batch_size 512 --lr 0.0001 --network_update_freq 10 --sync_target_network_freq 1000 --eps_decay 0.0001 --spawn_rate 10
