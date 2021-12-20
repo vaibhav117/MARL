8b0db902136487dadc829b3c61978b81596e7c08
vaibhav117 <vaibhavmathur117@gmail.com>
2021-12-19 23:28:01 -0500

    removing redundant property

--- config_script.sh
+++ config_script.sh
@@ -1,1 +1,1 @@
-python train.py --device cuda --project_name Deep_Learning_Project_2_multiagent --experiment_name Independent_DQNs --spawn_rate 1 --num_knights 2 --num_archers 2 --batch_size 512 --lr 0.0001 --network_update_freq 10 --sync_target_network_freq 1000 --eps_decay 0.0001 --spawn_rate 20
+python train.py --device cuda --project_name Deep_Learning_Project_2_multiagent --experiment_name Independent_DQNs --num_knights 2 --num_archers 2 --batch_size 512 --lr 0.0001 --network_update_freq 10 --sync_target_network_freq 1000 --eps_decay 0.0001 --spawn_rate 10
