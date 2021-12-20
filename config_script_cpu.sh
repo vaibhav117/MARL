97188b594af7ac34c44a0f8d01d5b7a3edcd3fd9
vaibhav117 <vaibhavmathur117@gmail.com>
2021-12-20 13:23:15 -0500

    final fixes

--- config_script_cpu.sh
+++ config_script_cpu.sh
@@ -1,1 +1,1 @@
-python train.py --device cpu --project_name Deep_Learning_Project_2_multiagent --experiment_name fullobs_Independent_DQNs --spawn_rate 1 --num_knights 2 --num_archers 2 --batch_size 256 --lr 0.0001 --network_update_freq 5 --sync_target_network_freq 500 --eps_decay 0.001 --spawn_rate 20
+python train.py --device cpu --project_name Deep_Learning_Project_2 --experiment_name fullobs_Independent_DQNs --spawn_rate 20 --num_knights 0 --num_archers 1 --batch_size 128 --lr 0.0001 --network_update_freq 5 --sync_target_network_freq 500 --eps_decay 0.001
